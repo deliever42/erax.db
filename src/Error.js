0e80d9bcbaaa9d93bfdc886cedda5cd6f58ce2bb
emirhanbaltas34 <77491112+emirhanbaltas34@users.noreply.github.com>
2021-08-29 20:28:59 +0300

    Delete src directory

--- src/Error.js
+++
@@ -1,9 +0,0 @@
-/**
- * Hata Yöneticisi
- * @private
- * @param {string} message Hata Mesajı
- * @returns {Error}
- */
-module.exports = (message) => {
-    throw new Error(`EraxDB => Bir Hata Oluştu: ${message}`);
-};
