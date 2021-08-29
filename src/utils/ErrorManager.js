8aa91aa5c36d26ace5c6cf0ecf2e1e7f52f18172
emirhanbaltas34 <77491112+emirhanbaltas34@users.noreply.github.com>
2021-08-29 20:30:17 +0300

    Add files via upload

--- src/utils/ErrorManager.js
+++ src/utils/ErrorManager.js
@@ -7,7 +7,7 @@ const chalk = require("chalk");
 module.exports = class ErrorManager extends Error {
     /**
      * @constructor
-     * @param {string} message
+     * @param {string} message Hata Mesajı
      */
     constructor(message) {
         super(`${chalk.blue("EraxDB")} => ${chalk.red("Bir Hata Oluştu:")} ${chalk.gray(message)}`);
