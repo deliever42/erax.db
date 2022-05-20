7d11284cfc5398d1e9b146c3832427ddfa3eb1ea
Emirhan <77491112+deliever42@users.noreply.github.com>
2022-05-21 00:09:17 +0300

    Delete src directory

--- src/Structures/Error/DatabaseError.js
+++
@@ -1,7 +0,0 @@
-const { red, gray, blue } = require('colorette');
-
-module.exports = class DatabaseError extends Error {
-    constructor(message) {
-        super(`${blue('[ EraxDB ]')} => ${red('Error:')} ${gray(message)}`);
-    }
-};
--- src/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    JsonDatabase: require('./Structures/Classes/Json'),
-    YamlDatabase: require('./Structures/Classes/Yaml'),
-    SqliteDatabase: require('./Structures/Classes/Sqlite'),
-    MongoDatabase: require('./Structures/Classes/Mongo')
-};
